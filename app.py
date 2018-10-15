3632a5c63ed23d1cc883a01830a62d6f9381cf9e
3reedm <nyuskov@newinfosec.ru>
2018-10-15 13:00:33 +0300

    Structure of the project and routing method were changed

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3.7
-
 import logging
-import sys
 
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-
-sys.path.append("./tornado-routing")
-import tokenizer.tokenizer
-
-from random import randint
-
-from tornado.options import define, options
-
-from tokenizer.tokenizer import app
-
-
-define("port", default=3000, help="run on the given port", type=int)
+from core.controller.controller import Controller
+from core.server.server import TornadoServer
+from core.client.client import Client
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-def main():
-    tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(app.get_application())
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+def __main__():
+    servers = [TornadoServer()]
+    clients = [Client()]
+
+    controller = Controller(servers[0], clients[0])
 
 
 if __name__ == '__main__':
-    main()
+    __main__()
